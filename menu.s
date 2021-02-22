7d08f303752ce0050069821ec6ec6caa91b53040
bruno185 <salut.lespotes@gmail.com>
2021-02-22 22:51:59 +0100

    c

--- menu.s
+++ menu.s
@@ -5,33 +5,33 @@ basl    equ $28
 cout    equ $FDED
 home    equ $FC58
 bascalc equ $FBC1
-wait    equ $FCA8
-timer   equ $01
 ptr1    equ $06
 prt2    equ $08
+ALTCHARSET0FF equ $C00E 
+ALTCHARSET0N equ $C00F
 
         org $4000
         jsr home
-        lda #$02
+        lda #$01        ; menu# to hilight
         sta curm
-        jsr dispmenu
-        rts
+        jsr dispmenu    ; display menu
+        rts             ; bye
 
 dispmenu nop
         ldx #$00
-loop    lda mtab,x
-        beq fin
-        pha             ; save meun#
-        inx 
-        lda mtab,x
+loop    lda mtab,x      ; read menu id
+        beq fin         ; if = 0 then end
+        pha             ; save meun #
+        inx             ; read string memory location
+        lda mtab,x      ; in ptr1
         sta ptr1
         inx
         lda mtab,x
         sta ptr1+1
-        pla             ; get menu#
-        cmp curm
-        bne prn
-        lda #$01
+        pla             ; get menu #
+        cmp curm        ; = menu # to hilight ?
+        bne prn         ; no : set flog to 0
+        lda #$01        ; yes : set flog to 1
         sta invflag
         jmp prn2
 prn     lda #$00
@@ -39,20 +39,28 @@ prn     lda #$00
 prn2    jsr print
         inx
         jmp loop
+        sta ALTCHARSET0FF
 fin     rts
 
 print   nop
+        sta ALTCHARSET0N        ; to get lowercase inverse
+* charsets explained here : 
+* https://retrocomputing.stackexchange.com/questions/8652/
         ldy #$00
-lprint  lda (ptr1),y
-        beq printend
-        ldx invflag
-        beq c
-        and #$7F
-c       jsr cout
+lprint  lda (ptr1),y    ; read menu string
+        beq printend    ; ended by 0
+        pha             ; save char. to display
+        lda invflag     ; normal or inverse ?
+        beq normal      ; normal char.
+        pla
+        and #$7F        ; inverse char.
+        jmp out
+normal  pla
+out     jsr cout
         iny
         jmp lprint
-printend nop
-        lda #$8D
+printend nop 
+        lda #$8D        ; next line
         jsr cout
         rts
 
@@ -64,16 +72,16 @@ mtab    hex 01
         da m3
         hex 04
         da m4
-        hex 00
+        hex 00          ; flog for end of array
 
 curm    hex 00
 invflag hex 00
 
-m1      asc "Item 1"
+m1      asc "item 1"
         hex 00
-m2      asc "Item 2"
+m2      asc "item 2"
         hex 00
-m3      asc "Item 3"
+m3      asc "item 3"
         hex 00
-m4      asc "Item 4"
+m4      asc "item 4"
         hex 00    
