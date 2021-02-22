17e358a6acfe02c6b0f087d4a9650256f63eacef
bruno185 <salut.lespotes@gmail.com>
2021-02-22 21:33:00 +0100

    c

--- doMerlin1.bat
+++ doMerlin1.bat
@@ -1,33 +1,27 @@
 @echo off
-if %errorlevel% neq 0 exit /b %errorlevel%
 echo --------------- Variables ---------------
 Set PRG=menu
-Set ProjectFolder=.
+Set ProjectFolder=C:\Users\Bruno\Documents\dev\Menu
 
-Set MyAppleFolder=F:\Bruno\Dev\AppleWin
+Set MyAppleFolder=C:\A
 Set APPLEWIN=%MyAppleFolder%\AppleWin\Applewin.exe
 Set MERLIN32ROOT=%MyAppleFolder%\Merlin32_v1.0
 Set MERLIN32LIBS=%MERLIN32ROOT%\Library
 Set MERLIN32WIN=%MERLIN32ROOT%\Windows
 Set MERLIN32EXE=%MERLIN32WIN%\merlin32.exe
-Set APPLECOMMANDER=%MyAppleFolder%\Utilitaires\AppleCommander-win64-1.6.0.jar
+Set APPLECOMMANDER=%MyAppleFolder%\AppleCommander-win64-1.6.0.jar
 rem Set ACJAR=java.exe -jar %APPLECOMMANDER%    ; avec ""
 Set ACJAR=java.exe -jar %APPLECOMMANDER%
-rem echo %ACJAR%
+echo %ACJAR%
 
 echo --------------- debut Merlin ---------------
 %MERLIN32EXE% -V %MERLIN32LIBS% %ProjectFolder%\%PRG%.s
-if exist %ProjectFolder%\error_output.txt exit
 echo --------------- fin Merlin ---------------
 
-copy /Y %MyAppleFolder%\A.po %ProjectFolder%\%PRG%.po
-
+copy /Y %MyAppleFolder%\AA.po %ProjectFolder%\%PRG%.po
+rem %ACJAR:"=% -p %PRG%.po %PRG% bin 24576 < %PRG%  ; suppression des "
 echo --------------- Debut Applecommander ---------------
-rem add binary program to image disk
 %ACJAR% -p %PRG%.po %PRG% bin 16384 < %PRG%
-rem add DUMMYFILE to image disk
-rem %ACJAR% -p %PRG%.po DUMMYFILE txt 0 < DUMMYFILE 
-
 echo --------------- fin Applecommander ---------------
 echo --------------- Debut Applewin ---------------
 %APPLEWIN% -d1 %PRG%.po
